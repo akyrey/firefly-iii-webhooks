701c56f0884778a3d1bf805966e77be0ea9bf5f7
akyrey <stein.dario@gmail.com>
2024-11-01 22:42:17 +0100

    refactor: split code and make it more generic

--- pkg/firefly/errors.go
+++ pkg/firefly/errors.go
@@ -3,6 +3,7 @@ package firefly
 import "errors"
 
 var (
+	ErrFireflyConfigNotFound   = errors.New("configuration not found")
 	ErrFireflyEmptyApiKey      = errors.New("api key cannot be empty")
 	ErrFireflyInvalidSignature = errors.New("invalid signature")
 	ErrFireflyInvalidSecret    = errors.New("invalid signature secret")
